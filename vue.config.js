c4da1ae8bab994616b8a15c6a1074757ee4d671b
Jan-Michael Löhlein <jan-michael.loehlein@hs-hannover.de>
2021-05-25 21:10:51 +0200

    Handle csp permissions with env files

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,16 @@ module.exports = {
   publicPath: "",
   outputDir: "www",
 
+  chainWebpack: (config) => {
+    config
+      .plugin('html')
+      .tap((args) => {
+        args[0].CSP_CONNECT_SOURCES = process.env.VUE_APP__ALLOWED_TO_CONNECT_TO;
+        args[0].DEV_SERVER_INLINE_EXECUTION_PERMISSIONS = process.env.NODE_ENV === "development"? "\'unsafe-inline\'": ""
+        return args;
+      });
+  },
+
 
   transpileDependencies: [
     'vuetify'
